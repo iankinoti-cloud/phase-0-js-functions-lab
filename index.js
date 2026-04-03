2d3cb9ae976a97ab9045023ad1545a05d92e3174
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-04-03 09:37:09 +0300

    correcting palindrome functions

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 
+function calculateTax(amount) {
+return amount * 0.10;
+}
+
+
+function convertToUpperCase(text) {
+return text.toUpperCase();
+}
 
 
 
+function findMaximum(num1, num2) {
+return num1 > num2 ? num1 : num2;
+}
 
-// This is required for the test to function properly  
+
+function isPalindrome(word) {
+const reversedWord = word.split('').reverse().join('');
+return word.toLowerCase() === reversedWord.toLowerCase();
+}
+
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+const discountAmount = originalPrice * (discountPercentage / 100);
+return originalPrice - discountAmount;
+}
+
+
+// This is required for the test to function properly
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+
