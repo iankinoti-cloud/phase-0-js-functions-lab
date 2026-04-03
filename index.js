a2ad7ffe02468f6518d8d26e08c9614869894a9c
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-04-03 09:38:08 +0300

    calulating discounted price auth

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ const discountAmount = originalPrice * (discountPercentage / 100);
 return originalPrice - discountAmount;
 }
 
-
 // This is required for the test to function properly
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
 
